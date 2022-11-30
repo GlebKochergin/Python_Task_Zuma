5d8c84051355dae067a9d4851fea426b36deaa83
Konstantin <zaikinke@yandex.ru>
2022-11-30 21:56:55 +0500

    Add hole and trajectory

--- levels/level.py
+++ levels/level.py
@@ -20,11 +20,28 @@ class Level1(Level):
         super().__init__(screen, back_menu)
         self.background = Background('src/backgrounds/level_1_background.jpg', [0, 0])
         self.frog = Frog(screen, 'src/textures/ArseniyFrog1.png')
+        self.ball = pygame.image.load('src/textures/GlebBall.png')
+        self.ball = pygame.transform.scale(self.ball, (60, 60))
+        self.hole = pygame.image.load('src/textures/hole.png')
+        self.hole = pygame.transform.scale(self.hole, (90, 90))
 
     def show(self, screen: pygame.Surface):
         while True:
             screen.fill([255, 255, 255])
             screen.blit(self.background.image, self.background.rect)
+            positions = [
+                [(55, 90, 1160, 65), (0, 100, 140, 140)],
+                [(55, 90, 65, 600), (1095, 165, 140, 140)],
+                [(55, 90, 960, 65), (135, 700, 140, 140)],
+                [(55, 90, 65, 300), (135, 400, 140, 140)]]
+            for pos in positions:
+                shape_surf = pygame.Surface(pygame.Rect(pos[0]).size,
+                                            pygame.SRCALPHA)
+                pygame.draw.rect(shape_surf, (255, 255, 255, 127),
+                                 shape_surf.get_rect())
+                screen.blit(shape_surf, pos[1])
+            screen.blit(self.ball, (200, 100))
+            screen.blit(self.hole, (122, 350))
             self.frog.rotate()
             self.frog.blit()
             for i in pygame.event.get():
